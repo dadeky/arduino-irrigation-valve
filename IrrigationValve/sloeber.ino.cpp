65ebcb79e4b862d0beb4e15678ededf1db86c7f5
dvorski <dadeky@gmail.com>
2018-06-22 21:43:14 +0200

    Some

--- IrrigationValve/sloeber.ino.cpp
+++ IrrigationValve/sloeber.ino.cpp
@@ -2,8 +2,9 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-12-28 13:51:00
+//This file has been generated on 2018-06-22 21:40:14
 
+#include "Arduino.h"
 #include "Arduino.h"
 
 void blinkLed();
